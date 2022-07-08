dbad3eddfab919c12bf2f6adf5b29498b0b0ebf8
Mateus Resende <mateus.resende.ext@sweetgreen.com>
2022-07-08 10:16:05 -0300

    add tests and isolate functionality of aged brie and sulfuras

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -19,6 +19,22 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
+
+            if ($item->name == 'Sulfuras, Hand of Ragnaros') {
+                continue;
+            }
+
+            if ($item->name == 'Aged Brie') {
+                if ($item->quality < 50) {
+                    $item->quality += 1;
+                }
+                $item->sell_in -= 1;
+                if ($item->sell_in < 0) {
+                    $item->quality += 1;
+                }
+                continue;
+            }
+
             if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if ($item->quality > 0) {
                     if ($item->name != 'Sulfuras, Hand of Ragnaros') {
--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -10,11 +10,43 @@ use PHPUnit\Framework\TestCase;
 
 class GildedRoseTest extends TestCase
 {
-    public function testFoo(): void
+    public function testAgedBrieShouldIncreaseQuality(): void
     {
-        $items = [new Item('foo', 0, 0)];
+        $items = [new Item('Aged Brie', 1, 0)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertSame('fixme', $items[0]->name);
+        $this->assertSame($items[0]->quality, 1);
+    }
+
+    public function testAgedBrieShouldIncreaseQualityByTwoIfSellInIsNegative(): void
+    {
+        $items = [new Item('Aged Brie', 0, 0)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame($items[0]->quality, 2);
+    }
+
+    public function testQualityCannotBeGreatedThan50(): void
+    {
+        $items = [new Item('Aged Brie', 1, 50)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame($items[0]->quality, 50);
+    }
+
+    public function testSulfurasDoesNotReduceQuality(): void
+    {
+        $items = [new Item('Sulfuras, Hand of Ragnaros', 1, 25)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame($items[0]->quality, 25);
+    }
+
+    public function testSulfurasDoesNotReduceSellIn(): void
+    {
+        $items = [new Item('Sulfuras, Hand of Ragnaros', 1, 25)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame($items[0]->sell_in, 1);
     }
 }
